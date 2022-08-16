ffccad65061de5421aa00afc089e0648b864757a
CloudBogdan <60233692+CloudBogdan@users.noreply.github.com>
2022-08-17 02:32:58 +0300

    Added .gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import Utils from "./utils/Utils.js";
-import Color from "./utils/Color.js";
-import Algorithms from "./utils/Algorithms.js";
-export * from "./core/Art.js";
-export { Utils, Color, Algorithms };
-//# sourceMappingURL=index.js.map
